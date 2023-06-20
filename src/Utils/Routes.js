f1272445228b7aea7b86d3554b9ee25729054ff2
FreakJazz <jazzrb2307@hotmail.com>
2023-06-19 21:43:18 -0500

    add submenu

--- src/Utils/Routes.js
+++ src/Utils/Routes.js
@@ -15,54 +15,90 @@ export const PATHS = {
 	OVERVIEW: '/overview',
 	LOGISTIC: '/logistic',
 	CLIENT: '/client',
+	CLIENT_LIST: '/client',
 	PRODUCT: '/product',
+	CLIENT_DETAILS: '/client/1',
+	PRODUCT_LIST: '/product',
+	PRODUCT_CREATE: '/product/new',
 	ORDER: '/order',
 };
 
 // This Array contains all the Routes for the App
+
+const clientItems = [
+	{
+	  title: 'list',
+	  path: PATHS.CLIENT_DETAILS,
+	},
+	{
+	  title: 'Detalles',
+	  path: PATHS.CLIENT_DETAILS,
+	},
+  ];
+
+const productItems =
+[
+	{
+	  title: 'list',
+	  path: PATHS.PRODUCT_LIST,
+	},
+	{
+	  title: 'create',
+	  path: PATHS.PRODUCT_CREATE,
+	},
+  ];
+
 export const ROUTES = [
 	{
 		title: 'Home',
 		path: PATHS.HOME,
 		component: Home,
+		items:  '',
 	},
 	{
 		title: 'register',
 		path: PATHS.REGISTER,
 		component: Register,
+		items:  '',
 	},
 
 	{
 		title: 'login',
 		path: PATHS.LOGIN,
 		component: Login,
+		items:  '',
 	},
 	{
 		title: 'overview',
 		path: PATHS.OVERVIEW,
 		component: Overview,
+		items:  '',
 	},
 
 	{
 		title: 'logistic',
 		path: PATHS.LOGISTIC,
 		component: Logistic,
+		items:  '',
 	},
 
 	{
 		title: 'client',
 		path: PATHS.CLIENT,
 		component: Client,
+		items: clientItems,
 	},
 
 	{
 		title: 'product',
 		path: PATHS.PRODUCT,
 		component: Product,
+		items: productItems,
 	},
 	{
 		title: 'order',
 		path: PATHS.ORDER,
 		component: Order,
+		items:  '',
 	},
 ];
